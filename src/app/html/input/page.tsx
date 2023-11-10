954a94b2470aec016df68d41c409f69a84fd8bdc
scRaul <61263078+scRaul@users.noreply.github.com>
2023-11-10 13:14:54 -0800

    input elements added

--- src/app/html/input/page.tsx
+++ src/app/html/input/page.tsx
@@ -4,7 +4,7 @@ import Collapsible from "@/components/CONTAINERS/Collapsible";
 export default function InputPage() {
   return (
     <>
-      <Collapsible className="w-full md:w-[750px]" label="<Button>">
+      <Collapsible className="w-full mb-4 md:w-[750px]" label="<Button>">
         <CodePreview title="button" code={``}>
           <button
             className="p-2 w-fit rounded border-2 bg-slate-100 hover:bg-slate-300  active:bg-slate-100"
@@ -31,7 +31,7 @@ export default function InputPage() {
         </CodePreview>
       </Collapsible>
 
-      <Collapsible className="w-full md:w-[750px]" label="<input>">
+      <Collapsible className="w-full mb-4  md:w-[750px]" label="<input>">
         <CodePreview title="button" code={``}>
           <input
             type="button"
@@ -59,10 +59,15 @@ export default function InputPage() {
           <input type="file" className="w-fit" />
         </CodePreview>
         <CodePreview title="hidden" code={``}>
-          <input type="hidden" className="w-fit" />
+          <input type="hidden" className="w-fit" value={"some_secrete"} />
+          <small>*used to store hidden data from user*</small>
         </CodePreview>
         <CodePreview title="image" code={``}>
-          <input type="image" className="w-fit" />
+          <input type="image" src="/imgBttn.png" className="w-24 border-2" />
+          <small>
+            *It's similar to the submit input type, but it allows you to use an
+            image as the button instead of text {"(its less common today)"}*
+          </small>
         </CodePreview>
         <CodePreview title="month" code={``}>
           <input type="month" className="w-fit" />
@@ -74,55 +79,96 @@ export default function InputPage() {
           <input type="password" className="w-fit  border-2" />
         </CodePreview>
         <CodePreview title="radio" code={``}>
-          <input type="radio" className="w-fit" />
+          <input id="radio" type="radio" className="w-fit" />
+          <label htmlFor="radio">live</label>
         </CodePreview>
         <CodePreview title="range" code={``}>
           <input type="range" className="w-fit" />
         </CodePreview>
         <CodePreview title="resest" code={``}>
-          <input type="reset" className="w-fit" />
+          <input
+            type="reset"
+            className="w-fit p-2 rounded bg-red-500 hover:bg-red-600 active:bg-red-500"
+          />
         </CodePreview>
         <CodePreview title="search" code={``}>
-          <input type="search" className="w-fit  border-2" />
+          <input type="search" className="w-fit border-2" />
         </CodePreview>
         <CodePreview title="submit" code={``}>
-          <input type="submit" className="w-fit" />
+          <input
+            type="submit"
+            className="w-fit p-2 rounded bg-blue-500 hover:bg-blue-600 active:bg-blue-500  "
+          />
         </CodePreview>
         <CodePreview title="tel" code={``}>
-          <input type="tel" className="w-fit" />
+          <input type="tel" className="w-fit border-2" />
         </CodePreview>
         <CodePreview title="text" code={``}>
-          <input type="text" className="w-fit" />
+          <input type="text" className="w-fit border-2" />
         </CodePreview>
         <CodePreview title="time" code={``}>
-          <input type="time" className="w-fit" />
+          <input type="time" className="w-fit border-2" />
         </CodePreview>
         <CodePreview title="url" code={``}>
-          <input type="url" className="w-fit" />
+          <input type="url" className="w-fit border-2" />
         </CodePreview>
         <CodePreview title="week" code={``}>
-          <input type="week" className="w-fit" />
+          <input type="week" className="w-fit border-2" />
         </CodePreview>
       </Collapsible>
 
-      <CodePreview title="<fieldset>" code={""}>
-        <div></div>
-      </CodePreview>
       <CodePreview title="<form>" code={""}>
-        <form></form>
+        <form>
+          <fieldset className="border-2 p-2 border-black">
+            <legend>Personal Information</legend>
+            <label htmlFor="name">Name:</label>
+            <input id="name" type="text" className="border-2 w-fit" />
+          </fieldset>
+          <fieldset className="border-2 p-2 border-black">
+            <legend>Meeting Date:</legend>
+            <label htmlFor="date">Select a date:</label>
+            <input id="date" type="date" className="border-2 w-fit" />
+          </fieldset>
+          <div className="flex justify-center gap-2 py-3">
+            <input
+              type="reset"
+              className="w-fit p-2 rounded bg-red-500 hover:bg-red-600 active:bg-red-500"
+            />
+            <input
+              type="submit"
+              className="w-fit p-2 rounded bg-blue-500 hover:bg-blue-600 active:bg-blue-500  "
+            />
+          </div>
+        </form>
       </CodePreview>
 
-      <CodePreview title="<label>" code={""}>
-        <div></div>
-      </CodePreview>
-      <CodePreview title="<legend>" code={""}>
-        <div></div>
-      </CodePreview>
       <CodePreview title="<search>" code={""}>
-        <div></div>
+        <search>
+          <form>
+            <label htmlFor="movie">Find a Movie</label>
+            <input
+              type="search"
+              id="movie"
+              name="q"
+              className="border-2 mx-2"
+            />
+            <button
+              type="submit"
+              className="mx-2 bg-yellow-200 hover:bg-yellow-300 border rounded p-1  border-black"
+            >
+              Search
+            </button>
+          </form>
+        </search>
       </CodePreview>
+
       <CodePreview title="<textarea>" code={""}>
-        <div></div>
+        <textarea
+          rows={4}
+          cols={80}
+          className="border-2"
+          placeholder="Type something..."
+        ></textarea>
       </CodePreview>
     </>
   );
