e2743d4806da26223395d28866368270b0d1e964
scRaul <61263078+scRaul@users.noreply.github.com>
2024-02-05 13:30:24 -0800

    updated topbar code

--- src/app/layout/topbar/page.tsx
+++ src/app/layout/topbar/page.tsx
@@ -22,57 +22,49 @@ function TopBarPage() {
 export default TopBarPage;
 
 const code: string = `//TopBar.tsx
-/*
-Use as a singleton instance ( edit this file to make changes )
-bar is split into three sections 
-left, mid , and right
-*/
 "use client";
-import { Shapes } from "lucide-react";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-
-function TopBar() {
-  const [scrolling, setScrolling] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 10) {
-        setScrolling(true);
-      } else {
-        setScrolling(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  const topBarClasses = \` \${
-    scrolling ? "bg-gray-500 fixed" : "bg-transparent"
-  } transition-all duration-300 ease-in-out\`;
-
+import { AlignJustify, Search, UserCircle } from "lucide-react";
+interface TopBarProps {
+  className?: string;
+}
+export default function TopBar(props: TopBarProps) {
   return (
-    <div
-      className={\`flex gap-1 justify-between items-center left-0 top-0 w-full p-3 pl-14 pr-10  h-14 \${topBarClasses} \`}
+    <header
+      className={\` flex p-2 gap-1 w-full min-h-fit items-center \${props.className}\`}
     >
-      <div id="left-section" className="flex w-full justify-start items-center">
-        <Link href="" className="cursor-pointer  flex ">
-          <Shapes />
-          UI Library
-        </Link>
+      <div className="pr-1 flex gap-2 items-center justify-between w-fit min-w-[150px]">
+        <div
+          className="rounded-full hover:bg-slate-200 p-2 cursor-pointer"
+          onClick={() => alert("open side bar / drop down pannel ")}
+        >
+          <AlignJustify />
+        </div>
+        <div
+          className="flex-grow cursor-pointer"
+          onClick={() => alert("back to home page")}
+        >
+          <img src="/next.svg"></img>
+        </div>
       </div>
-      <div id="mid-section" className="flex w-full justify-center items-center">
-        <p>topBar</p>
+      <div className="flex-grow"></div>
+      <div className="flex items-center justify-between px-1 gap-2 ">
+        <div
+          className="cursor-pointer"
+          onClick={() => alert("swap to search bar ")}
+        >
+          <Search />
+        </div>
+        <div
+          className="flex items-center justify-center border rounded-full p-1 px-2 hover:bg-slate-200 cursor-pointer"
+          onClick={() => alert("invoke login")}
+        >
+          <UserCircle />
+          <p>Sign In</p>
+        </div>
       </div>
-      <div id="right-section" className="flex w-full justify-end items-center">
-        <a className="text-blue-700 cursor-pointer">call-to-action</a>
-      </div>
-    </div>
+    </header>
   );
 }
-export default TopBar; 
+
 
 `;
